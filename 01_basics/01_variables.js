6b9bad5e99d2f258e97c18997e5e4343a6b5d327
deshpandeshefali <165694612+deshpandeshefali@users.noreply.github.com>
2024-04-02 15:04:05 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let accountName = "Shefali"
+let accountEmail = "s@gmail.com"
+
+accountCity = "Mumbai"
+console.table([accountEmail,accountId,accountName,accountCity])
+
+
+//prefer not to use var beacuse of issue in block scope and functional scope
