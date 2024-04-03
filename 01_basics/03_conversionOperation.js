ff2c5da42b77f4577713ecc4c5c1a4eb168beb0d
deshpandeshefali <165694612+deshpandeshefali@users.noreply.github.com>
2024-04-03 15:44:10 +0000

    jjhokjjjjjjjjj

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+/*let score = "33"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score) //to convert given datatype string to number
+console.log(typeof valueInNumber); // to identify the datatype of converted variable
+
+
+let score1 = "33abc"
+console.log(typeof score1)
+
+let valueInNumber1 = Number(score1) //to convert given datatype string to number
+console.log(typeof valueInNumber1); // to identify the datatype of converted variable
+console.log(valueInNumber1); // print the converted value. also used to check if the string-number value is converted to a number or not. Nan => Not a Number
+// null values converts to 0 => let score1 = null
+// undefined value is NaN after converting => let score1 = undefined
+*/
+/*
+let a = 33
+
+console.log(a);
+
+let valueInString = String(a)
+console.log(typeof valueInString);
+console.log(valueInString) 
+/*
+//"33" => 33
+//"33abc" => NaN
+
+/************************************************************************************************************************* */
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+*/
+
+//console.log("1" + 2 + 2); //everything is converted to a string
+//console.log(2 + 2 + "1"); // first number addition is carried out and then string is added 
+// for more info visit ecma 7. Abstract operations
+
+let number = 100
+number++ // result will be 101
+//++number // result will be 101
+console.log(number); 
+//more info visit postfix prefix js mdn
+
