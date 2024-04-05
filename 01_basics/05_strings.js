512e52f2483fd965e99fe02d1f6be3d3d0437c16
deshpandeshefali <165694612+deshpandeshefali@users.noreply.github.com>
2024-04-05 18:51:43 +0000

    kjjjjjjjj

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "shefali"
+const repoCount = 60
+
+//console.log(name + repoCount);
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const getName = new String("she-fa-li")
+/*
+console.log(getName[6]);
+console.log(getName.length);
+console.log(getName.toUpperCase());
+console.log(getName.charAt(4));
+console.log(getName.indexOf("f"));
+console.log(getName.toLowerCase());
+
+const newgetName = getName.substring(0,4)
+console.log(newgetName);
+//const anotherString = getName.slice(-4, 6)
+//console.log(anotherString);
+/*
+
+const newString = "                  shefali                  "
+console.log(newString);
+console.log(newString.trim());
+
+console.log(newString.trimEnd());
+console.log(newString.trimStart());
+*/
+const url = "https://shefali.com/shefali%20ddddd"
+console.log(url.replace('dd','6'));
+console.log(url.includes("sundar"));
+
+console.log(getName.split("-"));
